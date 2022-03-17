0f80b1e7a695ef012f01bfe743ef80368961557d
Omar Basheer <omarm.basheer@hotmail.com>
2022-03-17 15:11:11 +0400

    git remote add origin https://github.com/OmarBasheer/MOO_ESP.git git branch -M main git push -u origin main

--- main.py
+++ main.py
@@ -1,127 +1,56 @@
 from random import randint
+from support_methods import *
+import matplotlib.pyplot as plt
+import numpy
+import numpy as np
+import matplotlib
+
+###############
+
 
 num_sa = 36  # Number of appliances put to test
 num_slots = 1440  # Number of time slots, 1 minute each, demonstrating a full day
 all_sa = range(num_sa)
 all_slots = range(num_slots)
 remaining_slots = num_slots
-appliances = [  # [Appliance, LOC, OTPs, OTPe]
-              ['dw', 105, 540, 780], ['dw', 105, 840, 1080], ['dw', 105, 1200, 1440], ['ac', 30, 1, 120],
-              ['ac', 30, 120, 240], ['ac', 30, 240, 360], ['ac', 30, 360, 480], ['ac', 30, 480, 600],
-              ['ac', 30, 600, 720], ['ac', 30, 720, 840], ['ac', 30, 840, 960], ['ac', 30, 960, 1080],
-              ['ac', 30, 1080, 1200], ['ac', 30, 1200, 1320], ['ac', 30, 1320, 1440], ['wm', 55, 60, 300],
-              ['cd', 60, 300, 480], ['ref', 1440, 1, 1440], ['deh', 30, 1, 120], ['deh', 30, 120, 240],
-              ['deh', 30, 240, 360], ['deh', 30, 360, 480], ['deh', 30, 480, 600], ['deh', 30, 600, 720],
-              ['deh', 30, 720, 840], ['deh', 30, 840, 960], ['deh', 30, 960, 1080], ['deh', 30, 1080, 1200],
-              ['deh', 30, 1200, 1320], ['deh', 30, 1320, 1440], ['ewh', 35, 300, 420], ['ewh', 35, 1100, 1440],
-              ['cm', 10, 300, 450], ['cm', 10, 1020, 1140], ['pf', 180, 1, 540], ['pf', 180, 900, 1440]]
-
+appliances = getAppliances()
+ps_max_list = np.zeros(1441)
 
 positions = []
-
+consumption_per_min = []
 # randomize appliance within LB & (UB - l)
 for x in all_sa:
-    found = False
-    while not found:
-        if (appliances[x][3] - appliances[x][1]) >= appliances[x][2]:
-            slotter = randint(appliances[x][2], (appliances[x][3]-appliances[x][1]))
-        else:
-            slotter = appliances[x][2]
-        #  print(slotter)
-        positions.append([x, slotter, appliances[x]])
-        found = True
-
+    slotter = randint(appliances[x][2], appliances[x][3] - appliances[x][1])
+    positions.append([x, slotter, appliances[x]])  # [Appliance #, starting slot, appliance details]
+    for mint in range(slotter, slotter + appliances[x][1] + 1):
+        consumption_per_min.append([mint, appliances[x][4]])
+consumption_per_min.sort()
 
 print(positions)
-# Electricity bill = sum of power required for appliance * power consumption profile tariff at that slot of time PC =
-# a if  0 <= ps <= C || h if ps > C where C is consumption threshold between a & h, lambda positive ratio between 2
-#
-# tariffs h = lambda * a C = 0.0333 per time slot, lambda = 1.543 ps = {} pc = {} EB = sum(ps*pc)
+print(consumption_per_min)
 
-# prices per hour every 60 slots
-# 0-60 0.028
-# 61-120 0.023
-# 121-180 0.018
-# 181-240 0.013
-# 241-300 0.015
-# 301-360 0.022
-# 361-420 0.025
-# 421-480 0.035** 0.054
-# 481-540 0.04 0.062
-# 541-600 0.0416666 0.065
-# 601-660 0.045 0.069
-# 661-720 0.05 0.077
-# 721-780 0.051666 0.08
-# 781-840 0.053333 0.082
-# 841-900 0.055 0.085
-# 901-960 0.065 0.1
-# 961-1040 0.068 0.105
-# 1041-1100 0.06166 0.096
-# 1101-1160 0.053333 0.082
-# 1161-1200 0.051666 0.08
-# 1201-1260 0.05 0.077
-# 1261-1320 0.046666 0.073
-# 1321-1380 0.04 0.062
-# 1381-1440 0.031666**
+for x in consumption_per_min:
+    ps_max_list[x[0]] = ps_max_list[x[0]] + x[1]
 
-ap_cost = []
-# sum_cost = 0
-# fix boundaries
+print(ps_max_list)
 
-for x in positions:
-    # print(x)
-    for s in range(x[1], x[2][1]):
-        print(s)
+ps_max = ps_max_list[np.argmax(ps_max_list)]
+ps_avg = Average(ps_max_list)
 
 
-def price_calculate(minute):
-    sum_cost = 0
-    if 0 < minute <= 60:
-        sum_cost = sum_cost + 1
-    elif 60 < minute <= 120:
-        ap_cost.append(3)
-    elif 120 < minute < 180:
-        ap_cost.append(3)
-    elif 180 < minute <= 240:
-        ap_cost.append(3)
-    elif 240 < minute < 300:
-        ap_cost.append(3)
-    elif 300 < minute <= 360:
-        ap_cost.append(3)
-    elif 360 < minute < 420:
-        ap_cost.append(3)
-    elif 420 < minute <= 480:
-        ap_cost.append(3)
-    elif 480 < minute < 540:
-        ap_cost.append(3)
-    elif 540 < minute <= 600:
-        ap_cost.append(3)
-    elif 600 < minute < 660:
-        ap_cost.append(3)
-    elif 660 < minute <= 720:
-        ap_cost.append(3)
-    elif 720 < minute < 780:
-        ap_cost.append(3)
-    if 780 < minute <= 840:
-        ap_cost.append(3)
-    elif 840 < minute <= 900:
-        ap_cost.append(3)
-    elif 900 < minute < 960:
-        ap_cost.append(3)
-    elif 960 < minute <= 1020:
-        ap_cost.append(3)
-    elif 1020 < minute < 1080:
-        ap_cost.append(3)
-    elif 1080 < minute <= 1140:
-        ap_cost.append(3)
-    elif 1140 < minute < 1200:
-        ap_cost.append(3)
-    elif 1200 < minute <= 1260:
-        ap_cost.append(3)
-    elif 1260 < minute < 1320:
-        ap_cost.append(3)
-    elif 1320 < minute <= 1380:
-        ap_cost.append(3)
-    else:
-        ap_cost.append(3)
-    return sum_cost
+# get price per minute list
+price_per_min = getPricePerMin()
+
+eb_sum = 0
+ps_list = []
+ps_min = []
+for appliance in positions:  # Loop through all appliances
+    for minute in range(appliance[2][1]):  # Loop through durations of each appliance
+        ans, ps_list, ps_min = price_calculate(appliance[1] + minute,
+                                               appliance[2][4], ps_list, ps_min,
+                                               price_per_min)  # calculate the price based on starting slot + duration
+        eb_sum = eb_sum + ans
+print("Electricity Price is", eb_sum)
+
+par_val = ps_max / ps_avg
+wtr_avg_val = wtr_calc(positions)
---
+++ support_methods.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def Average(lst):
+    return sum(lst) / len(lst)
+
+def getAppliances():
+    appliances = [  # [Appliance, LOC, OTPs, OTPe, power usage in kW]
+        ['dw', 105, 540, 780, 1.5 / 60], ['dw', 105, 840, 1080, 1.5 / 60], ['dw', 105, 1200, 1440, 1.5 / 60],
+        ['ac', 30, 1, 120, 1.2 / 60],
+        ['ac', 30, 120, 240, 1.2 / 60], ['ac', 30, 240, 360, 1.2 / 60], ['ac', 30, 360, 480, 1.2 / 60],
+        ['ac', 30, 480, 600, 1.2 / 60],
+        ['ac', 30, 600, 720, 1.2 / 60], ['ac', 30, 720, 840, 1.2 / 60], ['ac', 30, 840, 960, 1.2 / 60],
+        ['ac', 30, 960, 1080, 1.2 / 60],
+        ['ac', 30, 1080, 1200, 1.2 / 60], ['ac', 30, 1200, 1320, 1.2 / 60], ['ac', 30, 1320, 1440, 1.2 / 60],
+        ['wm', 55, 60, 300, 1.15 / 60],
+        ['cd', 60, 300, 480, 5.4 / 60], ['ref', 1440, 0, 1440, 0.5 / 60], ['deh', 30, 1, 120, 0.65 / 60],
+        ['deh', 30, 120, 240, 0.65 / 60],
+        ['deh', 30, 240, 360, 0.65 / 60], ['deh', 30, 360, 480, 0.65 / 60], ['deh', 30, 480, 600, 0.65 / 60],
+        ['deh', 30, 600, 720, 0.65 / 60],
+        ['deh', 30, 720, 840, 0.65 / 60], ['deh', 30, 840, 960, 0.65 / 60], ['deh', 30, 960, 1080, 0.65 / 60],
+        ['deh', 30, 1080, 1200, 0.65 / 60],
+        ['deh', 30, 1200, 1320, 0.65 / 60], ['deh', 30, 1320, 1440, 0.65 / 60], ['ewh', 35, 300, 420, 4 / 60],
+        ['ewh', 35, 1100, 1440, 4 / 60],
+        ['cm', 10, 300, 450, 1.5 / 60], ['cm', 10, 1020, 1140, 1.5 / 60], ['pf', 180, 1, 540, 1 / 60],
+        ['pf', 180, 900, 1440, 1 / 60]]
+    return appliances
+
+def getPricePerMin():
+    price_per_min = np.zeros(1440)
+    price_per_min[0:61], price_per_min[61:121], price_per_min[121:181], price_per_min[
+                                                                        181:241] = 0.028, 0.023, 0.018, 0.013
+    price_per_min[241:301], price_per_min[301:361], price_per_min[361:421], price_per_min[
+                                                                            421:481] = 0.015, 0.022, 0.025, 0.054
+    price_per_min[481:541], price_per_min[541:601], price_per_min[601:661], price_per_min[
+                                                                            661:721] = 0.062, 0.065, 0.069, 0.077
+    price_per_min[721:781], price_per_min[781:841], price_per_min[841:901], price_per_min[
+                                                                            901:961] = 0.08, 0.082, 0.085, 0.1
+    price_per_min[961:1021], price_per_min[1021:1081], price_per_min[1081:1141], price_per_min[
+                                                                                 1141:1201] = 0.105, 0.096, 0.082, 0.08
+    price_per_min[1201:1261], price_per_min[1261:1321], price_per_min[1321:1381], price_per_min[
+                                                                                  1381:1440] = 0.077, 0.073, 0.062, 0.032
+    return price_per_min
+
+# Electricity bill = sum of power required for appliance * power consumption profile tariff at that slot of time PC =
+# a if  0 <= ps <= C || h if ps > C where C is consumption threshold between a & h, lambda positive ratio between 2
+#
+# tariffs h = lambda * a C = 0.0333 per time slot, lambda = 1.543 ps = {} pc = {} EB = sum(ps*pc)
+
+def price_calculate(min, price, ps_per_min, ps_min, price_per_min):  # change price to power consumption
+    sum_cost = price_per_min[min] * price
+    ps_per_min.append(price)
+    ps_min.append(min)
+    return sum_cost, ps_per_min, ps_min
+
+def wtr_calc(lst):
+    val1, val2 = 0, 0
+    for appliance in lst:
+        val1 = val1 + (appliance[1] - appliance[2][2])
+        val2 = val2 + (appliance[2][3] - appliance[2][2] - appliance[2][1])
+    return val1/val2
